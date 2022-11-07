4c558ed087179fedb3400c07cc465c8b791d4073
so <takaoso.st@gmail.com>
2022-11-07 15:41:54 +0000

    pushed demo notebook
    added docstrings

--- SparseOptimalInterpolation/models.py
+++ SparseOptimalInterpolation/models.py
@@ -9,9 +9,12 @@ from astropy.convolution import convolve, Gaussian2DKernel
 from typing import List, Dict
 
 
-#------- Base class ---------
+# ------- Base class ---------
 
 class SpatiotemporalOptimalInterpolation(ABC):
+    """
+    Base class containing the bare bone functionality for optimal interpolation using local GPs
+    """
     def __init__(self, training_data, gp_model, kernel, init_hyperparameters, mean_function=None, grid_res=50, training_radius=300*1000, time_window=9):
         X, z = training_data
         x, y, t = X.T
@@ -30,6 +33,9 @@ class SpatiotemporalOptimalInterpolation(ABC):
         self._init_hyperparameters = init_hyperparameters
 
     def _clip_and_smooth(self, param, vmin=0, vmax=1e10, std=1):
+        """
+        Apply clipping and Gaussian smoothing to a given hyperparameter field
+        """
         param = param.values # convert DataArray to numpy array
         # Clipping
         param[param >= vmax] = vmax
@@ -39,40 +45,69 @@ class SpatiotemporalOptimalInterpolation(ABC):
         return param
 
     def _post_process(self, param_fields: xr.Dataset, postprocess_kwargs):
+        """
+        Post process all trainable hyperparameters
+        """
         assert set(postprocess_kwargs.keys()) == set(self._hyperparam_keys)
         for key in self._hyperparam_keys:
             param = self._clip_and_smooth(param_fields.data_vars[key], **postprocess_kwargs[key])
             param_fields[key] = (('y', 'x'), param)
 
     @abstractmethod
-    def _get_hyperparameters(self, model=None):
+    def _get_hyperparameters(self, model):
         """
-        Fetch hyperparameters
+        Method to get the hyperparameters of the model
+
+        Output:
+        Hyperparameters of the model given by a dictionary whose keys are the hyperparameter labels
+        and the values are the corresponding values
         """
 
     @abstractmethod
     def _get_kernel(self):
         """
-        Instantiate kernel
+        Method to instantiate kernel
         """
 
     @abstractmethod
     def _get_mean(self):
-        ...
+        """
+        Method to get the mean function
+        """
 
     @abstractmethod
     def _build_model(self, X, y, kernel, mean, hyperparameters):
-        ...
+        """
+        Method to compile GP model
+        """
 
     @abstractmethod
     def _optimise(self, model):
-        ...
+        """
+        Method to optimise hyperparameters of the model
+        """
 
     @abstractmethod
     def _get_prediction(self, model, gridpoint) -> List:
-        ...
+        """
+        Method to get the predicted mean and variance of the model
+
+        Output: List of form (mean, variance)
+        """
 
     def train(self, dates, region: xr.Dataset, trainable_hyperparameters: List, postprocess_kwargs=None):
+        """
+        Main training loop.
+        ------
+        Args
+        ------
+        :dates: A list containing the dates of consideration
+        :region: An xarray Dataset object containing coordinates x and y for the region of consideration
+        :trainable_hyperparameters: A list containing the names of the trainable hyperparameters.
+                                    The names must be compatible with the keys in self._init_hyperparameters
+        :postprocess_kwargs: A nested dictionary of form
+                            {hyperparameter name : dictionary of key word arguments to feed into self._clip_and_smooth}
+        """
         self._hyperparam_keys = trainable_hyperparameters
         for date in dates:
             print(f"Training on date: {date}")
@@ -82,8 +117,12 @@ class SpatiotemporalOptimalInterpolation(ABC):
             y_coords = region['y'].values
             data_vars = {key: (('y', 'x'), np.ones((ydim, xdim))) for key in trainable_hyperparameters}
             hyperparam_fields = xr.Dataset(data_vars=data_vars, coords={'x': x_coords, 'y': y_coords})
+            count = 0
+            cumulative_time = 0
             for i, x in enumerate(x_coords):
                 for j, y in enumerate(y_coords):
+                    time0 = time.time()
+
                     # 1. Select training data within a ball of radius r of the current grid point
                     ID = self.kdt_tree.query_ball_point(x=np.array([x,y]).T, r=self.radius)
                     inputs = np.array([self.x_train[ID], self.y_train[ID], self.t_train[ID]]).T
@@ -94,7 +133,7 @@ class SpatiotemporalOptimalInterpolation(ABC):
                     mean = self._get_mean()
                     model = self._build_model(inputs, outputs, kernel, mean, self._init_hyperparameters)
 
-                    # 3. Set trainable hyperparameters
+                    # 3. Set trainable hyperparameters (TODO: Fix. Not general)
                     hyperparams = self._get_hyperparameters(model)
                     for key, val in hyperparams.items():
                         if key not in trainable_hyperparameters:
@@ -102,11 +141,16 @@ class SpatiotemporalOptimalInterpolation(ABC):
 
                     # 4. Optimise hyperparameters
                     self._optimise(model)
-
+                    
                     # 5. Cache optimised hyperparameters
                     hyperparams_new = self._get_hyperparameters(model)
                     for key in trainable_hyperparameters:
                         hyperparam_fields.data_vars[key][j,i] = hyperparams_new[key]
+
+                    time1 = time.time()
+                    cumulative_time += time1-time0
+                    count += 1
+                    print(f"Time elapsed for gridpoint {count}/{xdim*ydim}: {time1-time0:.3f}s")
 
             # Post-process hyperparameters
             print(f"Post-processing hyperparameters...")
@@ -114,10 +158,17 @@ class SpatiotemporalOptimalInterpolation(ABC):
                 self._post_process(hyperparam_fields, postprocess_kwargs)
 
             # Log results
+            print(f"Saving results...")
             path = f"log/hyperparameters_{date}_{self.resolution}km.nc" # TODO: add logdir to class attribute
             hyperparam_fields.to_netcdf(path)
 
+            print(f"Complete.")
+            print(f"Total time for training: {cumulative_time:.3f}s")\
+
     def predict(self, dates, region: xr.Dataset):
+        """
+        Make predictions
+        """
         T_mid = self.T // 2 # choose central day in the time window to make predictions
         x_coords = region['x'].values
         y_coords = region['y'].values
@@ -128,7 +179,7 @@ class SpatiotemporalOptimalInterpolation(ABC):
             hyperparam_dataset = xr.load_dataset(path)
             for i, x in enumerate(x_coords):
                 for j, y in enumerate(y_coords):
-                    # TODO: Steps 1 and 2 below are repeated from training. Add as separate method.
+                    # TODO: Steps 1 and 2 below are almost identical to that in training step. Add as separate method.
                     # 1. Select training data within a ball of radius r of the current grid point
                     ID = self.kdt_tree.query_ball_point(x=np.array([x,y]).T, r=self.radius)
                     inputs = np.array([self.x_train[ID], self.y_train[ID], self.t_train[ID]]).T
@@ -150,11 +201,11 @@ class SpatiotemporalOptimalInterpolation(ABC):
 
         return gridded_mean, gridded_var
 
-#------- Pure python model ---------
+# ------- Pure python model ---------
 
 ...
 
-#------- GPflow models ---------
+# ------- GPflow models ---------
 
 class GPR_OptimalInterpolation(SpatiotemporalOptimalInterpolation):
     def __init__(self, training_data, kernel, init_hyperparameters, max_iter=10_000, grid_res=50, training_radius=300*1000, time_window=9):
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='SparseOptimalInterpolation',
+    packages=find_packages(),
+)
