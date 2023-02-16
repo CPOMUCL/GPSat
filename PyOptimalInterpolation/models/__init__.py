e370a5ca007cf1a19dd23370b908758cdf13d8d3
so <takaoso.st@gmail.com>
2023-02-16 15:51:54 +0000

    fixed models init file

--- PyOptimalInterpolation/models/__init__.py
+++ PyOptimalInterpolation/models/__init__.py
@@ -1,3 +1,7 @@
 from PyOptimalInterpolation.models.base_model import BaseGPRModel
 from PyOptimalInterpolation.models.gpflow_models import GPflowGPRModel, GPflowSGPRModel
-from PyOptimalInterpolation.models.sklearn_models import sklearnGPRModel
+try:
+    from PyOptimalInterpolation.models.sklearn_models import sklearnGPRModel
+    from PyOptimalInterpolation.models.vff_model import GPflowVFFModel
+except:
+    pass
