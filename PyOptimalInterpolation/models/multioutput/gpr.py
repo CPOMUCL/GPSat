3d256a53be8edea05ba4d75cdb15670aa12cc6b9
so <takaoso.st@gmail.com>
2023-03-23 00:18:27 +0000

    refactored multioutput

--- PyOptimalInterpolation/models/multioutput/gpr.py
+++ PyOptimalInterpolation/models/multioutput/gpr.py
@@ -1,24 +1,14 @@
-import numpy as np
-from math import sqrt
-import matplotlib.pyplot as plt
 import gpflow
-from gpflow.likelihoods import Gaussian, Likelihood
-from gpflow.models import GPR, GPModel
-from gpflow.utilities.parameter_or_function import (
-    ConstantOrFunction,
-    ParameterOrFunction,
-    evaluate_parameter_or_function,
-    prepare_parameter_or_function,
-)
-from gpflow.models.util import data_input_to_tensor
-from gpflow.utilities import add_likelihood_noise_cov
+from gpflow.models import GPR, GPModel, SVGP
+from gpflow.models.util import data_input_to_tensor, InducingVariablesLike
+from gpflow.kernels import LinearCoregionalization
 from gpflow.logdensities import multivariate_normal
 from gpflow.mean_functions import MeanFunction, Zero
-from check_shapes import check_shapes, inherit_check_shapes
+from check_shapes import inherit_check_shapes
 import tensorflow as tf
-from typing import Optional
-from copy import copy
-from likelihoods import LinearModelLikelihood
+from likelihoods import LinearModelLikelihood, NonlinearModelLikelihood, ForwardModelLikelihood
+from utils import add_likelihood_noise_cov, multioutput_conditional
+from typing import Union, Optional
 
 
 class MultioutputGPR(GPR):
@@ -46,89 +36,6 @@ class MultioutputGPR(GPR):
         if mean_function is None:
             self.mean_function = Zero(output_dim=Y_data.shape[-1])
 
-    @staticmethod
-    @check_shapes(
-        "K: [batch..., N, N, P, P]",
-        "return: [batch..., N, N, P, P]",
-    ) # TODO: Simplify by taking shape [N, P, N, P] instead
-    def add_likelihood_noise_cov(K, likelihood: LinearModelLikelihood):
-        R = likelihood.variance # Shape (P, P)
-        N, P = K.shape[-3], K.shape[-1]
-        # Construct a block-diagonal matrix of R's to add to K
-        R_op = tf.linalg.LinearOperatorFullMatrix(R)
-        R_op_diag = tf.linalg.LinearOperatorBlockDiag([R_op for _ in range(N)]) # Shape (N*P, N*P)
-        # Reshape K to tensor of hape (..., N*P, N*P)
-        axis_indices = [i for i in range(len(K.shape))]
-        leading_indices, tensor_indices = axis_indices[:-4], axis_indices[-4:]
-        tensor_indices_permuted = [tensor_indices[0], tensor_indices[2], tensor_indices[1], tensor_indices[3]]
-        K = tf.transpose(K, leading_indices+tensor_indices_permuted) # Shape (..., N, P, N, P)
-        leading_dims = K.shape[:-4]
-        K = tf.reshape(K, leading_dims+[N*P, N*P]) # Shape (..., N*P, N*P)
-        # Add R's to the diagonal of K and reshape
-        KnR = R_op_diag.add_to_tensor(K) # Shape (..., N*P, N*P)
-        KnR = tf.reshape(KnR, leading_dims+[N, P, N, P]) # Shape (..., N, P, N, P)
-        return tf.transpose(KnR, leading_indices+tensor_indices_permuted) # Shape (..., N, N, P, P)
-
-    @check_shapes(
-        "Kmn: [M, L, N, L]",
-        "Kmm: [M, L, M, L]",
-        "Knn: [N, L, N, L] if full_cov",
-        "Knn: [N, L, L] if not full_cov",
-        "f: [M, P]",
-        "return[0]: [N, L]",
-        "return[1]: [N, L, N, L] if full_cov",
-        "return[1]: [N, L, L] if not full_cov",
-    )
-    def multioutput_conditional(self,
-                                Kmn: tf.Tensor, # Shape (M, L, N, L)
-                                Kmm: tf.Tensor, # Shape (M, L, M, L)
-                                Knn: tf.Tensor, # Shape (N, L, N, L)
-                                f: tf.Tensor, # Shape (M, P)
-                                *,
-                                full_cov=False):
-
-        Kmn = tf.transpose(Kmn, [0,2,1,3]) # Shape (M, N, L, L)
-        Kmm = tf.transpose(Kmm, [0,2,1,3]) # Shape (M, M, L, L)
-        H = self.likelihood.H
-
-        HKmmHt = H @ Kmm @ tf.transpose(H) # Shape (M, M, P, P)
-        HKmn = H @ Kmn # Shape (M, N, P, L)
-        ks = self.add_likelihood_noise_cov(HKmmHt, self.likelihood) # Shape (M, M, P, P)
-        ks = tf.transpose(ks, [0,2,1,3]) # Shape (M, P, M, P)
-        N, P, M, _ = ks.shape
-        ks = tf.reshape(ks, (M*P, M*P)) # Reshape to size (MP, MP)
-        Lm = tf.linalg.cholesky(ks) # Shape (MP, MP)
-
-        # Compute the projection matrix A
-        HKmn = tf.transpose(HKmn, [0,2,1,3]) # Shape (M, P, N, L)
-        M, P, N, L = HKmn.shape
-        HKmn = tf.reshape(HKmn, (M*P, N*L)) # Reshape to size (MP, NL)
-        A = tf.linalg.triangular_solve(Lm, HKmn, lower=True)  # Shape (MP, NL)
-
-        # Compute conditional covariance
-        if full_cov:
-            Knn = tf.reshape(Knn, (N*L, N*L))
-            fvar = Knn - tf.linalg.matmul(A, A, transpose_a=True) # Shape (NL, NL)
-            fvar = tf.reshape(fvar, (N, L, N, L))
-        else:
-            def square(tensor):
-                """Input is a tensor of shape (M, P, L) """
-                tensor = tf.reshape(tensor, (M*P, L)) # Reshape to size (MP, L)
-                return tf.linalg.matmul(tensor, tensor, transpose_a=True) # Shape (L, L)
-
-            A = tf.reshape(A, (M, P, N, L))
-            AtA_diag = tf.stack([square(A[:,:,n,:]) for n in range(N)], 0) # Shape (N, L, L)
-            fvar = Knn - AtA_diag
-            A = tf.reshape(A, (M*P, N*L))
-
-        # Compute conditional mean
-        f = tf.reshape(f, (M*P,1)) # Shape (MP, 1)
-        A = tf.linalg.triangular_solve(tf.linalg.adjoint(Lm), A, lower=False) # Shape (MP, NL)
-        fmean = tf.linalg.matmul(A, f, transpose_a=True) # Shape (NL, 1)
-        fmean = tf.reshape(fmean, (N,L)) # Shape (N, L)
-
-        return fmean, fvar
-
     @inherit_check_shapes
     def log_marginal_likelihood(self) -> tf.Tensor:
         X, Y = self.data
@@ -136,7 +43,7 @@ class MultioutputGPR(GPR):
         K = self.kernel(X, full_cov=True, full_output_cov=True) # Shape (N, L, N, L)
         K = tf.transpose(K, [0,2,1,3]) # Shape (N, N, L, L)
         HKHt = H @ K @ tf.transpose(H) # Shape (N, N, P, P)
-        ks = self.add_likelihood_noise_cov(HKHt, self.likelihood) # Shape (N, N, P, P)
+        ks = add_likelihood_noise_cov(HKHt, self.likelihood.variance) # Shape (N, N, P, P)
         ks = tf.transpose(ks, [0,2,1,3]) # Shape (N, P, N, P)
         N, P = ks.shape[:2]
         ks = tf.reshape(ks, (N*P, N*P)) # Shape (NP, NP)
@@ -159,8 +66,8 @@ class MultioutputGPR(GPR):
         knn = self.kernel(Xnew, full_cov=full_cov, full_output_cov=True) # Shape (N, L, N, L)
         kmn = self.kernel(X, Xnew, full_cov=True, full_output_cov=True) # Shape (M, L, N, L)
 
-        f_mean_zero, f_var = self.multioutput_conditional(
-            kmn, kmm, knn, err, full_cov=full_cov
+        f_mean_zero, f_var = multioutput_conditional(
+            kmn, kmm, knn, err, H=self.likelihood.H, R=self.likelihood.variance, full_cov=full_cov
         )  # [N, P], [N, P] or [P, N, N]
 
         f_mean = f_mean_zero + self.mean_function(Xnew)
@@ -171,7 +78,42 @@ class MultioutputGPR(GPR):
         return f_mean, f_var
 
 
-class MultioutputSVGP(gpflow.models.SVGP):
+class MultioutputSVGP(SVGP):
+    def __init__(
+        self,
+        kernel: LinearCoregionalization,
+        likelihood: ForwardModelLikelihood,
+        inducing_variable: InducingVariablesLike,
+        *,
+        mean_function: Optional[MeanFunction] = None,
+        q_diag: bool = False,
+        q_mu: Optional[tf.Tensor] = None,
+        q_sqrt: Optional[tf.Tensor] = None,
+        num_data: Optional[tf.Tensor] = None,
+    ):
+        """
+        - kernel, likelihood, inducing_variables, mean_function are appropriate
+            GPflow objects
+        - q_diag is a boolean. If True, the covariance is approximated by a
+            diagonal matrix.
+        - whiten is a boolean. If True, we use the whitened representation of
+            the inducing points.
+        - num_data is the total number of observations, defaults to X.shape[0]
+            (relevant when feeding in external minibatches)
+        """
+        assert isinstance(kernel, LinearCoregionalization)
+        assert isinstance(likelihood, ForwardModelLikelihood)
+
+        super().__init__(kernel,
+                         likelihood,
+                         inducing_variable,
+                         mean_function=mean_function,
+                         num_latent_gps=likelihood.latent_dim,
+                         q_diag=q_diag,
+                         q_mu=q_mu,
+                         q_sqrt=q_sqrt,
+                         num_data=num_data)
+
     @inherit_check_shapes
     def elbo(self, data) -> tf.Tensor:
         """
@@ -180,13 +122,7 @@ class MultioutputSVGP(gpflow.models.SVGP):
         """
         X, Y = data
         kl = self.prior_kl()
-        if isinstance(self.likelihood, LinearModelLikelihood):
-            full_cov = False
-            full_output_cov = True
-        else:
-            full_cov = False
-            full_output_cov=False
-        f_mean, f_cov = self.predict_f(X, full_cov=full_cov, full_output_cov=full_output_cov)
+        f_mean, f_cov = self.predict_f(X, full_cov=False, full_output_cov=True)
         var_exp = self.likelihood.variational_expectations(X, f_mean, f_cov, Y)
         if self.num_data is not None:
             num_data = tf.cast(self.num_data, kl.dtype)
@@ -195,4 +131,174 @@ class MultioutputSVGP(gpflow.models.SVGP):
         else:
             scale = tf.cast(1.0, kl.dtype)
         return tf.reduce_sum(var_exp) * scale - kl
+
+
+if __name__ == "__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from utils import multivariate_gaussian_log_density, ForwardModel
+    from copy import copy
+
+    xs = np.arange(0, 5, 0.1)
+    ys = np.arange(0, 5, 0.1)
+
+    Xs, Ys = np.meshgrid(xs, ys)
+
+    xdim, ydim = Xs.shape
+
+    X = np.stack([Xs.ravel(), Ys.ravel()], axis=1)
+
+    # Generate toy data
+    kern1 = gpflow.kernels.Matern52()
+    kern2 = gpflow.kernels.Matern52()
+    kern3 = gpflow.kernels.Matern52(lengthscales=10)
+
+    K1xx = kern1(X, X)
+    K2xx = kern2(X, X)
+    K3xx = kern3(X, X)
+
+    np.random.seed(1)
+    W = np.random.randn(xdim, ydim, 3)
+
+    Chol1 = np.linalg.cholesky(K1xx)
+    g1 = Chol1 @ W[...,0].ravel()
+
+    Chol2 = np.linalg.cholesky(K2xx)
+    g2 = Chol2 @ W[...,1].ravel()
+
+    Chol3 = np.linalg.cholesky(K3xx)
+    g3 = Chol3 @ W[...,2].ravel()
+
+    # Independent latent GPs
+    g1 = g1.reshape(xdim, ydim)
+    g2 = g2.reshape(xdim, ydim)
+    g3 = g3.reshape(xdim, ydim)
+
+    # Mixed latent GPs
+    mix_mat = np.array([[0.7, 0.3, 0.2], [0.3, 0.7, 0.1], [0.2, 0.1, 0.7]])
+    # mix_mat = np.array([[0.7, 0.3, 0.], [0.3, 0.7, 0.], [0., 0., 0.7]])
+
+    g123 = np.stack([g1, g2, g3], axis=2)[..., None] # Shape (X, Y, 3, 1)
+    f123 = mix_mat[None, None] @ g123 # Shape (X, Y, 3, 1)
+    f123 = f123[...,0] # Shape (X, Y, 3)
+
+    f1 = f123[..., 0]
+    f2 = f123[..., 1]
+    f3 = f123[..., 2]
+
+    # Generate observations
+    def sigmoid(x):
+        return 1/(1 + np.exp(-x))
+
+    def forward(f):
+        y1 = sigmoid(f[...,2]) * f[...,0] + 0.5 * f[...,1]
+        y2 = f[...,1]
+        return np.stack([y1, y2], axis=-1)
+
+    c = 0.5
+    y1 = sigmoid(f3) * f1 + 0.5 * f2
+    y2 = f2[...]
+
+    num_obs = 200
+    obs_noise = 1e-2
+
+    rng = np.random.default_rng(0)
+    x_idxs = np.arange(xdim)
+    y_idxs = np.arange(ydim)
+    X_idxs, Y_idxs = np.meshgrid(x_idxs[1:-1], y_idxs[1:-1], indexing='ij')
+    all_idxs = np.stack([X_idxs.flatten(), Y_idxs.flatten()], axis=1)
+    idxs = rng.choice(all_idxs, num_obs, replace=False)
+
+    x_train = [X.reshape(xdim, ydim, 2)[tuple(idx)] for idx in idxs]
+    y_train = [(forward(f123[tuple(idx)])).squeeze() + np.sqrt(obs_noise)*np.random.randn(2) for idx in idxs]
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+
+    class ExampleModel(ForwardModel):
+        def __init__(self, c, *, input_dim, latent_dim, observation_dim):
+            super().__init__(input_dim=input_dim, latent_dim=latent_dim, observation_dim=observation_dim)
+            self.c = c
+
+        @inherit_check_shapes
+        def _forward(self, X, F):
+            sigmoid = tf.math.sigmoid
+            y1 = sigmoid(F[...,2]) * F[...,0] + self.c * F[...,1]
+            y2 = F[...,1]
+            return tf.stack([y1, y2], axis=-1)
+
+
+    # Create list of kernels for each output
+    kern_list = [
+        gpflow.kernels.Matern52(),
+        gpflow.kernels.Matern52(),
+        gpflow.kernels.Matern52(lengthscales=10)
+    ]
+    # Create multi-output kernel from kernel list
+    kernel = gpflow.kernels.LinearCoregionalization(
+        kern_list, W=mix_mat
+    )  # Notice that we initialise the mixing matrix W
+    # initialisation of inducing input locations (M random points from the training inputs)
+    M = 200
+    Z = copy(x_train)
+    np.random.shuffle(Z)
+    Z = Z[:M]
+    # create multi-output inducing variables from Z
+    iv = gpflow.inducing_variables.SharedIndependentInducingVariables(
+        gpflow.inducing_variables.InducingPoints(Z)
+    )
+
+    forward_map = ExampleModel(c=0.5, input_dim=2, latent_dim=3, observation_dim=2)
+    likelihood = NonlinearModelLikelihood(forward_map, 1e-2, num_samples=1000)
+
+    # initialize mean of variational posterior to be of shape MxL
+    q_mu = np.zeros((M, 3))
+    # initialize \sqrt(Σ) of variational posterior to be of shape LxMxM
+    q_sqrt = np.repeat(np.eye(M)[None, ...], 3, axis=0) * 1.0
+
+    # create SVGP model as usual and optimize
+    m = MultioutputSVGP(
+        kernel,
+        likelihood,
+        inducing_variable=iv,
+        q_mu=q_mu,
+        q_sqrt=q_sqrt,
+    )
+
+    gpflow.utilities.set_trainable(m.kernel.W, False)
+    gpflow.utilities.set_trainable(m.likelihood.variance, False) 
+
+    def optimize_model_with_adam(model, data, iterations, minibatch_size=128):
+        (X, Y) = data
+        train_dataset = tf.data.Dataset.from_tensor_slices((X, Y)).repeat().shuffle(X.shape[0])
+        train_iter = iter(train_dataset.batch(minibatch_size))
+        training_loss = model.training_loss_closure(train_iter, compile=True)
+        optimizer = tf.optimizers.Adam()
+        @tf.function
+        def optimization_step():
+            optimizer.minimize(training_loss, model.trainable_variables)
+        for step in range(iterations):
+            if step % 100 == 0:
+                print(f"step: {step}, elbo: {-training_loss().numpy()}")
+            optimization_step()
+
+    optimize_model_with_adam(m, (x_train, y_train), 4000)
+
+    f_mean, f_var = m.predict_f(X)
+
+    f_mean = tf.reshape(f_mean, (xdim, ydim, 3))
+
+    fig, axs = plt.subplots(2,3)
+    axs[0,0].imshow(f1)
+    axs[0,0].set_title('f1 true')
+    axs[0,1].imshow(f2)
+    axs[0,1].set_title('f2 true')
+    axs[0,2].imshow(f3)
+    axs[0,2].set_title('f3 true')
+    axs[1,0].imshow(f_mean[...,0])
+    axs[1,0].set_title('f1 prediction')
+    axs[1,1].imshow(f_mean[...,1])
+    axs[1,1].set_title('f2 prediction')
+    axs[1,2].imshow(f_mean[...,2])
+    axs[1,2].set_title('f3 prediction')
+    plt.tight_layout()
 
--- PyOptimalInterpolation/models/multioutput/likelihoods.py
+++ PyOptimalInterpolation/models/multioutput/likelihoods.py
@@ -1,60 +1,60 @@
 import numpy as np
-from math import sqrt
-import matplotlib.pyplot as plt
-import gpflow
-from gpflow.likelihoods import Gaussian, Likelihood
-from gpflow.models import GPR, GPModel
-from gpflow.utilities.parameter_or_function import (
-    ConstantOrFunction,
-    ParameterOrFunction,
-    evaluate_parameter_or_function,
-    prepare_parameter_or_function,
-)
-from gpflow.models.util import data_input_to_tensor
-from gpflow.utilities import add_likelihood_noise_cov
-from gpflow.logdensities import multivariate_normal
-from gpflow.mean_functions import MeanFunction, Zero
-from check_shapes import check_shapes, inherit_check_shapes
 import tensorflow as tf
-from typing import Optional
-from copy import copy
-from gpflow.likelihoods import MonteCarloLikelihood
-from dataclasses import dataclass
+import gpflow
+from gpflow.likelihoods import Likelihood, MonteCarloLikelihood
+from gpflow.utilities.parameter_or_function import prepare_parameter_or_function
+from check_shapes import check_shapes, inherit_check_shapes
+from utils import multivariate_gaussian_log_density, ForwardModel, LinearForwardModel
 from abc import ABC, abstractmethod
+from typing import Union
 
 
-class LinearModelLikelihood(Likelihood):
-    """
-    Implements likelihood of form y = Hx + noise
-    """
-    def __init__(self, input_dim, variance, H):
-        observation_dim, latent_dim = H.shape
-        super().__init__(input_dim=input_dim, latent_dim=latent_dim, observation_dim=observation_dim)
-        
+# TODO: Rename variance -> obs_covariance / R?
+
+class ForwardModelLikelihood(ABC):
+    def __init__(self, variance: tf.Tensor, *args, **kwargs):
+        self.h = self.get_model(*args, **kwargs)
+        assert isinstance(self.h, ForwardModel)
+
+        observation_dim = self.h.observation_dim
         if isinstance(variance, (int, float)):
             variance = variance*tf.eye(observation_dim, dtype=tf.float64)
             
         assert variance.shape == (observation_dim, observation_dim)
 
         self.variance = prepare_parameter_or_function(variance) # Converts variance to gpflow parameter
-        self.H = H
 
-    @staticmethod
-    @check_shapes(
-        "x: [broadcast shape..., input_dim]",
-        "mu: [broadcast shape..., P]",
-        "cov: [broadcast shape..., P, P]",
-        "return: [shape...]",
-    )
-    def multivariate_gaussian_log_density(x, mu, cov) -> tf.Tensor:
-        P = mu.shape[-1]
-        diff = tf.expand_dims(mu-x, axis=-1) # (..., P, 1)
-        shape = tf.concat([tf.shape(mu)[:-1], [P, P]], axis=0)
-        cov_broadcast = tf.broadcast_to(cov, shape) # (..., P, P)
-        out = - (P/2) * np.log(2 * np.pi) \
-              - 0.5 * tf.linalg.logdet(cov) \
-              - 0.5 * tf.linalg.matmul(diff, tf.linalg.solve(cov_broadcast, diff), transpose_a=True) # (..., 1, 1)
-        return tf.reduce_sum(out, [-1,-2]) # (..., 1, 1) -> (...)
+    @abstractmethod
+    def get_model(self, *args, **kwargs) -> ForwardModel:
+        return NotImplementedError
+
+
+class LinearModelLikelihood(Likelihood, ForwardModelLikelihood):
+    """
+    Implements likelihood of form y = Hx + noise
+    """
+    def __init__(self,
+                 input_dim: int,
+                 variance: tf.Tensor,
+                 forward_model: Union[tf.Tensor, LinearForwardModel]):
+        
+        ForwardModelLikelihood.__init__(self, variance, input_dim, forward_model)
+        Likelihood.__init__(self,
+                            input_dim=self.h.input_dim,
+                            latent_dim=self.h.latent_dim,
+                            observation_dim=self.h.observation_dim)
+
+        self.H = self.h.tensor
+
+    def get_model(self, input_dim, forward_model):
+        if isinstance(forward_model, (tf.Tensor, np.ndarray)):
+            observation_dim, latent_dim = forward_model.shape
+            H = forward_model
+            return LinearForwardModel(input_dim, latent_dim, observation_dim, H)
+        elif isinstance(forward_model, LinearForwardModel):
+            return forward_model
+        else:
+            return TypeError
 
     @check_shapes(
         "X: [batch..., N, D]",
@@ -68,13 +68,13 @@ class LinearModelLikelihood(Likelihood):
 
     @inherit_check_shapes
     def _log_prob(self, X, F, Y) -> tf.Tensor:
-        HF = tf.linalg.matvec(self.H, F)
+        HF = self.h(X, F)
         # return gpflow.logdensities.gaussian(Y, HF, self.variance) # Note: only deals with scalar y
-        return self.multivariate_gaussian_log_density(Y, HF, self.variance)
+        return multivariate_gaussian_log_density(Y, HF, self.variance)
 
     @inherit_check_shapes
     def _conditional_mean(self, X, F) -> tf.Tensor:
-        HF = tf.linalg.matvec(self.H, F)
+        HF = self.h(X, F)
         return tf.identity(HF)
 
     @inherit_check_shapes
@@ -90,8 +90,8 @@ class LinearModelLikelihood(Likelihood):
         "return[1]: [batch..., observation_dim, observation_dim]",
     )
     def _predict_mean_and_var(self, X, Fmu, Fcov) -> tf.Tensor:
-        HFmu = tf.linalg.matvec(self.H, Fmu)
-        HFcovHt = self.H @ Fcov @ tf.transpose(self.H)
+        HFmu = self.h.propagate_mean(Fmu)
+        HFcovHt = self.h.propagate_cov(Fcov)
         return tf.identity(HFmu), HFcovHt + self.variance
 
     def predict_mean_and_var(self, X, Fmu, Fcov):
@@ -104,9 +104,9 @@ class LinearModelLikelihood(Likelihood):
         "return: [batch...]",
     )
     def _predict_log_density(self, X, Fmu, Fcov, Y) -> tf.Tensor:
-        HFmu = tf.linalg.matvec(self.H, Fmu) # Shape (..., P)
-        HFcovHt = self.H @ Fcov @ tf.transpose(self.H) # Shape (..., P, P)
-        return self.multivariate_gaussian_log_density(Y, HFmu, HFcovHt + self.variance)
+        HFmu = self.h.propagate_mean(Fmu) # Shape (..., P)
+        HFcovHt = self.h.propagate_cov(Fcov) # Shape (..., P, P)
+        return multivariate_gaussian_log_density(Y, HFmu, HFcovHt + self.variance)
 
     def predict_log_density(self, X, Fmu, Fcov, Y):
         return self._predict_log_density(X, Fmu, Fcov, Y)
@@ -118,8 +118,8 @@ class LinearModelLikelihood(Likelihood):
         "return: [batch...]",
     )
     def _variational_expectations(self, X, Fmu, Fcov, Y) -> tf.Tensor:
-        HFmu = tf.linalg.matvec(self.H, Fmu) # Shape (..., P)
-        HFcovHt = self.H @ Fcov @ tf.transpose(self.H) # Shape (..., P, P)
+        HFmu = self.h.propagate_mean(Fmu) # Shape (..., P)
+        HFcovHt = self.h.propagate_cov(Fcov) # Shape (..., P, P)
         covariance = self.variance # (P, P)
         shape = tf.concat([tf.shape(HFmu)[:-1], tf.shape(covariance)], axis=0)
         cov_broadcast = tf.broadcast_to(covariance, shape) # (..., P, P)
@@ -135,46 +135,106 @@ class LinearModelLikelihood(Likelihood):
         return self._variational_expectations(X, Fmu, Fcov, Y)
 
 
-class ForwardModel(ABC):
-    def __init__(self, input_dim, latent_dim, observation_dim, *args, **kwargs):
-        self.input_dim = input_dim
-        self.latent_dim = latent_dim
-        self.observation_dim = observation_dim
-    
-    @abstractmethod
-    @check_shapes(
-        "X: [batch..., input_dim]",
-        "F: [batch..., latent_dim]",
-        "return: [batch..., observation_dim]",
-    )
-    def _forward(self, X, F):
-        pass
-
-    def __call__(self, X, F):
-        return self._forward(X, F)
-
-
-class NonlinearModelLikelihood(MonteCarloLikelihood):
+class NonlinearModelLikelihood(MonteCarloLikelihood, ForwardModelLikelihood):
     def __init__(self,
-                 forward_map: ForwardModel,
+                 forward_model: ForwardModel,
                  variance: tf.Tensor,
                  num_samples: int=100) -> None:
 
-        super().__init__(input_dim=forward_map.input_dim,
-                         latent_dim=forward_map.latent_dim,
-                         observation_dim=forward_map.observation_dim)
+        MonteCarloLikelihood.__init__(self,
+                                      input_dim=forward_model.input_dim,
+                                      latent_dim=forward_model.latent_dim,
+                                      observation_dim=forward_model.observation_dim)
 
-        self.h = forward_map
-        if isinstance(variance, (int, float)):
-            variance = variance*tf.eye(self.observation_dim, dtype=tf.float64)
-            
-        assert variance.shape == (self.observation_dim, self.observation_dim)
+        ForwardModelLikelihood.__init__(self, variance, forward_model)
 
-        self.variance = prepare_parameter_or_function(variance) # Converts variance to gpflow parameter
         self.num_monte_carlo_points = num_samples
+
+    def get_model(self, forward_model):
+        return forward_model
+
+    @check_shapes(
+        "Fmu: [batch..., latent_dim]",
+        "Fcov: [batch..., latent_dim, latent_dim]",
+        "Ys.values(): [batch..., .]",
+        "return: [broadcast n_funcs, batch..., .]",
+    ) # 
+    def _mc_quadrature(self, func, Fmu, Fcov, **Ys) -> tf.Tensor:
+        N, D = tf.shape(Fmu)[0], tf.shape(Fcov)[-1]
+        S = self.num_monte_carlo_points
+        epsilon = tf.random.normal(shape=[S, N, D], dtype=tf.float64)
+        L = tf.linalg.cholesky(Fcov)
+        mc_x = Fmu[None, :, :] + tf.reduce_mean(L[None, :, :, :] @ epsilon[..., None], -1)
+        mc_Xr = tf.reshape(mc_x, (S * N, D))
+        for name, Y in Ys.items():
+            D_out = Y.shape[1]
+            mc_Yr = tf.tile(Y[None, ...], [S, 1, 1])  # [S, N, D_out]
+            Ys[name] = tf.reshape(mc_Yr, (S * N, D_out))  # [S * N, D_out]
+        feval = func(mc_Xr, **Ys)
+        feval = tf.reshape(feval, (S, N, -1))
+        return tf.reduce_mean(feval, axis=0)
 
     @inherit_check_shapes
     def _log_prob(self, X, F, Y) -> tf.Tensor:
         Y_pred = self.h(X, F)
-        return LinearModelLikelihood.multivariate_gaussian_log_density(Y, Y_pred, self.variance)
+        return multivariate_gaussian_log_density(Y, Y_pred, self.variance)
+
+    @check_shapes(
+        "X: [broadcast batch..., input_dim]",
+        "Fmu: [broadcast batch..., latent_dim]",
+        "Fcov: [broadcast batch..., latent_dim, latent_dim]",
+        "return: [batch...]",
+    )
+    def _variational_expectations(self, X, Fmu, Fcov, Y) -> tf.Tensor:
+        def log_prob(F, X_, Y_) -> tf.Tensor:
+            return self.log_prob(X_, F, Y_)
+
+        return tf.reduce_sum(
+            self._mc_quadrature(log_prob, Fmu, Fcov, X_=X, Y_=Y), axis=-1
+        )
+    
+    def variational_expectations(self, X, Fmu, Fcov, Y):
+        return self._variational_expectations(X, Fmu, Fcov, Y)
+   
+        
+if __name__ == "__main__":
+
+    # Test
+    lik = LinearModelLikelihood(input_dim=2, variance=0.1, forward_model=tf.constant([[0.5, 0.5]], dtype=tf.float64))
+
+    f = tf.constant([[1., 1.]], dtype=tf.float64)
+    fmu = tf.constant([[1., 1.]], dtype=tf.float64)
+    fvar = tf.eye(2, dtype=tf.float64)[None]
+    X = tf.constant([[0.5, 0.5]], dtype=tf.float64)
+    y = tf.constant([[1.]], dtype=tf.float64)
+
+    print(lik.conditional_mean(X, f))
+    print(lik.conditional_variance(X, f))
+    print(lik.predict_mean_and_var(X, fmu, fvar))
+    print(lik.predict_log_density(X, fmu, fvar, y))
+    print(lik.variational_expectations(X, fmu, fvar, y))
+
+    class ExampleModel(ForwardModel):
+        def __init__(self, c, *, input_dim, latent_dim, observation_dim):
+            super().__init__(input_dim=input_dim, latent_dim=latent_dim, observation_dim=observation_dim)
+            self.c = c
+
+        @inherit_check_shapes
+        def _forward(self, X, F):
+            sigmoid = tf.math.sigmoid
+            y1 = sigmoid(F[...,2]) * F[...,0] + self.c * F[...,1]
+            y2 = F[...,1]
+            return tf.stack([y1, y2], axis=-1)
+
+    forward_map = ExampleModel(c=0.5, input_dim=2, latent_dim=3, observation_dim=2)
+    lik = NonlinearModelLikelihood(forward_map, variance=0.1, num_samples=1000)
+
+    f = tf.constant([[1., 1., 1.]], dtype=tf.float64)
+    fmu = tf.constant([[1., 1., 1.]], dtype=tf.float64)
+    fvar = tf.eye(3, dtype=tf.float64)[None]
+    X = tf.constant([[0.5, 0.5]], dtype=tf.float64)
+    y = tf.constant([[1., 1.]], dtype=tf.float64)
+
+    print(lik.variational_expectations(X, fmu, fvar, y))
+
 
