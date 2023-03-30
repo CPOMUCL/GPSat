0eaca3547720046a9f752c55099fe0415746d882
ronaldBuddys <ronaldmaceachern@gmail.com>
2023-03-30 09:08:35 +0100

    update requirements, models __init__ to except Exceptions

--- PyOptimalInterpolation/models/__init__.py
+++ PyOptimalInterpolation/models/__init__.py
@@ -2,16 +2,19 @@ from PyOptimalInterpolation.models.base_model import BaseGPRModel
 
 try:
     from PyOptimalInterpolation.models.gpflow_models import GPflowGPRModel, GPflowSGPRModel, GPflowSVGPModel
-except:
+except Exception as e:
+    print(f"Exception:\n{e}\noccurred while trying to import: GPflowGPRModel, GPflowSGPRModel")
     print("Could not load GPflow models. Check if GPflow 2 is installed")
 
 try:
     from PyOptimalInterpolation.models.sklearn_models import sklearnGPRModel
-except:
+except Exception as e:
+    print(f"Exception:\n{e}\noccurred while trying to import: sklearnGPRModel")
     print("Could not load sklearn model. Check if scikit-learn is installed")
 
 try:
     from PyOptimalInterpolation.models.vff_model import GPflowVFFModel
-except:
+except Exception as e:
+    print(f"Exception:\n{e}\noccurred while trying to import: GPflowVFFModel")
     print("Could not load VFF model. Check if GPflow 2 is installed")
 
