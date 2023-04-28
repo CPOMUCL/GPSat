82e0b546b7283a7776f8f5b5196379bb3bbeacf8
ronaldBuddys <ronaldmaceachern@gmail.com>
2023-04-28 13:18:51 +0100

    add docstrings to datetime_utils, add unit tests for datetime_utils

--- PyOptimalInterpolation/datetime_utils.py
+++ PyOptimalInterpolation/datetime_utils.py
@@ -10,17 +10,39 @@ import pandas as pd
 
 def from_file_start_end_datetime_GPOD(f, df):
     """
-    parse file name to get start and end datetimes
-    - assume observations occur at equally spaced intervals over
+    Extract an implied sequence of evenly spaced time intervals based off of a 'processed' GPOD file name
+
+    This function takes in a file path and a pandas dataframe as input.
+    It extracts the start and end datetime from the file name and
+    calculates the time interval between them.
+
+    It then generates a datetime array with the same length as the dataframe,
+    evenly spaced over the time interval. The resulting datetime array is returned.
 
     Parameters
     ----------
-    f: str, filename
-    df: pd.DataFrame
+    f: str
+        filename
+    df: pd.DataFrame, pd.Series, np.array, tuple, list
+        the len(df) is used to determine the number and size of the intervals
+
 
     Returns
     -------
-    np.array, dtype datetime64[ns]
+    np.array
+        dtype datetime64[ns]
+
+    Examples
+    --------
+    >>> f = "/path/to/S3A_GPOD_SAR__SRA_A__20191031T233355_20191101T002424_2019112_IL_v3.proc"
+    >>> df = pd.DataFrame({"x": np.arange(11)})
+    >>> from_file_start_end_datetime_GPOD(f, df)
+    array(['2019-10-31T23:33:55.000000000', '2019-10-31T23:38:57.900000000',
+           '2019-10-31T23:44:00.800000000', '2019-10-31T23:49:03.700000000',
+           '2019-10-31T23:54:06.600000000', '2019-10-31T23:59:09.500000000',
+           '2019-11-01T00:04:12.400000000', '2019-11-01T00:09:15.300000000',
+           '2019-11-01T00:14:18.200000000', '2019-11-01T00:19:21.100000000',
+           '2019-11-01T00:24:24.000000000'], dtype='datetime64[ns]')
 
     """
 
@@ -32,7 +54,14 @@ def from_file_start_end_datetime_GPOD(f, df):
     dt0 = np.datetime64(datetime.datetime.strptime(dt0, "%Y%m%dT%H%M%S"))
     dt1 = np.datetime64(datetime.datetime.strptime(dt1, "%Y%m%dT%H%M%S"))
 
-    delta_t = (dt1 - dt0) / len(df)
+    assert isinstance(df, (pd.DataFrame, pd.Series, np.ndarray, list, tuple)), \
+        f"df is wrong type, got: {type(df)}"
+    assert len(df) > 0, "df has length 0"
+
+    _ = (len(df) - 1)
+    _ = 1 if _ == 0 else _
+
+    delta_t = (dt1 - dt0) / _
 
     dt = dt0 + np.arange(len(df)) * delta_t
 
@@ -43,8 +72,43 @@ def from_file_start_end_datetime_GPOD(f, df):
 
 
 def from_file_start_end_datetime_SARAL(f, df):
+    """
+    This function takes in a file path to a file and
+    a pandas dataframe and returns a numpy array of datetime objects.
 
-    # TODO: add doc string
+    The file path is expected to be in the format of SARAL data files,
+    with the datetime information encoded in the file name.
+    The function extracts the start and end datetime information from the file name,
+    calculates the time interval between them based on the length of the dataframe, and
+    generates a numpy array of datetime objects with the same length as the dataframe.
+
+    Parameters
+    ----------
+    f: str
+        the file path of the SARAL data file
+    df: pd.DataFrame
+        the data contained in the SARAL data file
+
+    Returns
+    -------
+    np.array
+        datetime objects, representing the time stamps of the data in the SARAL data file
+        with dtype: 'datetime64[s]'
+
+    Examples
+    --------
+    >>> f = "/path/to/SARAL_C139_0036_20200331_234125_20200401_003143_CS2mss_IL_v1.proc"
+    >>> df = pd.DataFrame({"x": np.arange(11)})
+    >>> from_file_start_end_datetime_SARAL(f, df)
+    array(['2020-03-31T23:41:25', '2020-03-31T23:46:26',
+           '2020-03-31T23:51:28', '2020-03-31T23:56:30',
+           '2020-04-01T00:01:32', '2020-04-01T00:06:34',
+           '2020-04-01T00:11:35', '2020-04-01T00:16:37',
+           '2020-04-01T00:21:39', '2020-04-01T00:26:41',
+           '2020-04-01T00:31:43'], dtype='datetime64[s]')
+
+
+    """
     f = os.path.basename(f)
     # example file name: SARAL_C139_0036_20200331_234125_20200401_003143_CS2mss_IL_v1.proc
     # found: /cpdata/SATS/RA/ALTIKA/SGDR_f/processed/north/by_month/YYYYMM/
@@ -60,7 +124,13 @@ def from_file_start_end_datetime_SARAL(f, df):
     dt0 = np.datetime64(datetime.datetime.strptime(date0+time0, "%Y%m%d%H%M%S"))
     dt1 = np.datetime64(datetime.datetime.strptime(date1+time1, "%Y%m%d%H%M%S"))
 
-    delta_t = (dt1 - dt0) / len(df)
+    assert isinstance(df, (pd.DataFrame, pd.Series, np.ndarray, list, tuple)), f"df is wrong type, got: {type(df)}"
+    assert len(df) > 0, "df has length 0"
+
+    _ = (len(df) - 1)
+    _ = 1 if _ == 0 else _
+
+    delta_t = (dt1 - dt0) / _
 
     dt = dt0 + np.arange(len(df)) * delta_t
 
@@ -71,13 +141,75 @@ def from_file_start_end_datetime_SARAL(f, df):
 
 
 def datetime_from_float_column(float_datetime, epoch=(1950, 1, 1), time_unit='D'):
+    """
+    Converts a float datetime column to a datetime64 format.
 
+    Parameters
+    -----------
+    float_datetime : pd.Series or np.array
+        A pandas series or numpy array containing float values, corresponding to datetime.
+    epoch : tuple, default is (1950, 1, 1).
+        A tuple representing the epoch date in the format (year, month, day).
+    time_unit : str, optional
+        The time unit of the float datetime values. Default is 'D' (days).
+
+    Returns
+    --------
+    numpy.ndarray
+        A numpy array of datetime64 values, with dtype 'datetime64[s]'
+
+    Example
+    --------
+    >>> df = pd.DataFrame({'float_datetime': [18262.5, 18263.5, 18264.5]})
+    >>> datetime_from_float_column(df['float_datetime'])
+    array(['2000-01-01T12:00:00', '2000-01-02T12:00:00',
+           '2000-01-03T12:00:00'], dtype='datetime64[s]')
+
+    >>> df = pd.DataFrame({'float_datetime': [18262.5, 18263.5, 18264.5]})
+    >>> datetime_from_float_column(df['float_datetime'], epoch=(1970, 1, 1))
+    array(['2020-01-01T12:00:00', '2020-01-02T12:00:00',
+           '2020-01-03T12:00:00'], dtype='datetime64[s]')
+
+    >>> x = np.array([18262.5, 18263.5, 18264.5])
+    >>> datetime_from_float_column(x, epoch=(1970, 1, 1))
+    array(['2020-01-01T12:00:00', '2020-01-02T12:00:00',
+           '2020-01-03T12:00:00'], dtype='datetime64[s]')
+    """
     # convert float_datetime to a timedelta (time since epoch)
     dt = pd.to_timedelta(float_datetime, unit=time_unit) + datetime.datetime(*epoch)
     return dt.values.astype('datetime64[s]')
 
 
+# TODO: remove date_from_datetime - not used in package
 def date_from_datetime(dt):
+    """
+    Remove the time component of an array of datetimes (represented as strings) and just return the date
+
+    The datetime format is expected to be YYYY-MM-DD HH:mm:SS
+    The returned date format is YYYYMMDD
+
+    Parameters
+    ----------
+    dt: list, np.array, pd.Series
+        string with datetime format YYYY-MM-DD HH:mm:SS.
+
+    Returns
+    -------
+    numpy.ndarray: A date column with format YYYY-MM-DD.
+
+    Example
+    -------
+        >>> dt = pd.Series(['2022-01-01 12:00:00', '2022-01-02 13:00:00', '2022-01-03 14:00:00'])
+        >>> date_from_datetime(dt)
+        array(['20220101', '20220102', '20220103'], dtype='<U8')
+
+    Note
+    ----
+    This function uses a lambda function to remove the time portion and the dash from the datetime column.
+    It then returns a numpy array of the resulting date column.
+    It is possible to use apply on a Series to achieve the same result,
+    but it may not be as fast as using a lambda function and numpy array.
+    """
     # convert a datetime column with format YYYY-MM-DD HH:mm:SS
     # would it be faster use apply on a Series?
     remove_dash_and_time = lambda x: re.sub(" .*$|-", "", x)
@@ -85,6 +217,42 @@ def date_from_datetime(dt):
 
 
 def datetime_from_ymd_cols(year, month, day, hhmmss):
+    """
+    Converts separate columns/arrays of year, month, day, and time (in hhmmss format)
+    into a numpy array of datetime objects.
+
+    Parameters
+    ----------
+    year : array-like
+        An array of integers representing the year.
+    month : array-like
+        An array of integers representing the month (1-12).
+    day : array-like
+        An array of integers representing the day of the month.
+    hhmmss : array-like
+        An array of integers representing the time in hhmmss format.
+
+    Returns
+    -------
+    datetime : numpy.ndarray
+        An array of datetime objects representing the input dates and times.
+
+    Raises
+    ------
+    AssertionError
+        If the input arrays are not of equal length.
+
+    Example:
+    --------
+    >>> year = [2021, 2021, 2021]
+    >>> month = [1, 2, 3]
+    >>> day = [10, 20, 30]
+    >>> hhmmss = [123456, 234537, 165648]
+    >>> datetime_from_ymd_cols(year, month, day, hhmmss)
+    array(['2021-01-10T12:34:56', '2021-02-20T23:45:37',
+           '2021-03-30T16:56:48'], dtype='datetime64[s]')
+
+    """
 
     # NOTE: the following is likely slow...
     # check sizes
@@ -104,5 +272,4 @@ def datetime_from_ymd_cols(year, month, day, hhmmss):
 if __name__ == "__main__":
 
     pass
-
 
---
+++ tests/test_datetime_utils.py
@@ -0,0 +1,323 @@
+# unit test for functions in datetime_utils
+
+import pytest
+import numpy as np
+import pandas as pd
+import datetime
+
+from PyOptimalInterpolation.datetime_utils import from_file_start_end_datetime_SARAL, \
+    datetime_from_ymd_cols, datetime_from_float_column, from_file_start_end_datetime_GPOD
+
+# -----
+# datetime_from_float_column
+# -----
+
+@pytest.mark.parametrize(
+    "float_datetime, epoch, time_unit, expected",
+    [
+        # Test basic functionality with default epoch and time_unit
+        (
+            pd.Series([18262.5, 18263.5, 18264.5]),
+            (1950, 1, 1),
+            'D',
+            np.array(['2000-01-01T12:00:00', '2000-01-02T12:00:00', '2000-01-03T12:00:00'], dtype='datetime64[s]')
+        ),
+        # Test basic functionality with non-default epoch and time_unit
+        (
+            pd.Series([18262.5, 18263.5, 18264.5]),
+            (1970, 1, 1),
+            'D',
+            np.array(['2020-01-01T12:00:00', '2020-01-02T12:00:00', '2020-01-03T12:00:00'], dtype='datetime64[s]')
+        ),
+        # Test input with NaN values
+        (
+            pd.Series([np.nan, 18262.5, 18263.5]),
+            (1950, 1, 1),
+            'D',
+            np.array(['NaT', '2000-01-01T12:00:00', '2000-01-02T12:00:00'], dtype='datetime64[s]')
+        ),
+        # Test input with negative float values
+        (
+            pd.Series([-1.5, 0.0, 1.5]),
+            (1950, 1, 1),
+            'D',
+            np.array(['1949-12-30T12:00:00', '1950-01-01T00:00:00', '1950-01-02T12:00:00'], dtype='datetime64[s]')
+        ),
+        # Test input with very large float values
+        (
+            pd.Series([1e9, 2e9, 3e9]),
+            (1950, 1, 1),
+            's',
+            np.array(['1981-09-09T01:46:40', '2013-05-18T03:33:20',
+                      '2045-01-24T05:20:00'], dtype='datetime64[s]')
+        ),
+        # Test input with float values outside of the representable range for datetime64
+        (
+            pd.Series([1e100, -1e100]),
+            (1950, 1, 1),
+            'D',
+            np.array(['NaT', 'NaT'], dtype='datetime64[s]')
+        ),
+        # Test with basic input
+        (pd.Series([18262.5, 18263.5, 18264.5]), (1950, 1, 1), 'D',
+         np.array(['2000-01-01T12:00:00', '2000-01-02T12:00:00', '2000-01-03T12:00:00'], dtype='datetime64[s]')),
+
+        # Test with different epoch
+        (pd.Series([18262.5, 18263.5, 18264.5]), (1970, 1, 1), 'D',
+         np.array(['2020-01-01T12:00:00', '2020-01-02T12:00:00', '2020-01-03T12:00:00'], dtype='datetime64[s]')),
+
+        # Test with numpy array input
+        (np.array([18262.5, 18263.5, 18264.5]), (1970, 1, 1), 'D',
+         np.array(['2020-01-01T12:00:00', '2020-01-02T12:00:00', '2020-01-03T12:00:00'], dtype='datetime64[s]')),
+
+        # Test with empty input
+        (pd.Series([]), (1970, 1, 1), 'D', np.array([], dtype='datetime64[s]')),
+
+        # Test with time_unit in hours
+        (pd.Series([438001.0, 438025.0, 438049.0]), (1970, 1, 1), 'h',
+         np.array(['2019-12-20T01:00:00', '2019-12-21T01:00:00', '2019-12-22T01:00:00'], dtype='datetime64[s]')),
+
+        # Test with time_unit in minutes
+        (pd.Series([26340161.0, 26340441.0, 26340721.0]), (1970, 1, 1), 'm',
+         np.array(['2020-01-30T18:41:00', '2020-01-30T23:21:00', '2020-01-31T04:01:00'], dtype='datetime64[s]')),
+
+        # Test with time_unit in seconds
+        (pd.Series([1577920680, 1578007080, 1578093480]), (1970, 1, 1), 's',
+         np.array(['2020-01-01T23:18:00', '2020-01-02T23:18:00', '2020-01-03T23:18:00'], dtype='datetime64[s]')),
+
+    ]
+)
+def test_datetime_from_float_column(float_datetime, epoch, time_unit, expected):
+    """
+    Test the datetime_from_float_column function for various inputs and expected outputs
+    """
+
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            result = datetime_from_float_column(float_datetime, epoch=epoch, time_unit=time_unit)
+    else:
+        # Run the function
+        result = datetime_from_float_column(float_datetime, epoch=epoch, time_unit=time_unit)
+        # Compare the result to the expected output
+        np.testing.assert_array_equal(result, expected)
+
+
+@pytest.mark.parametrize("float_datetime, epoch, time_unit, expected_exception", [
+    # Test with invalid time_unit
+    (pd.Series([18262.5, 18263.5, 18264.5]), (1970, 1, 1), 'X', ValueError),
+
+    # Test with invalid epoch
+    (pd.Series([18262.5, 18263.5, 18264.5]), '1970-01-01', 'D', TypeError),
+
+    # Test with invalid float_datetime input
+    (['2020-01-01T12:00:00', '2020-01-02T12:00:00', '2020-01-03T12:00:00'], (1970, 1, 1), 'D', ValueError),
+])
+def test_datetime_from_float_column_errors(float_datetime, epoch, time_unit, expected_exception):
+    with pytest.raises(expected_exception):
+        datetime_from_float_column(float_datetime, epoch, time_unit)
+
+# -----
+# datetime_from_ymd_cols
+# -----
+
+@pytest.mark.parametrize(
+    "year, month, day, hhmmss, expected",
+    [
+        # Test basic functionality
+        (
+                [2021, 2021, 2021],
+                [1, 2, 3],
+                [10, 20, 30],
+                [123456, 234537, 165648],
+                np.array(['2021-01-10T12:34:56', '2021-02-20T23:45:37', '2021-03-30T16:56:48'], dtype='datetime64[s]')
+        ),
+        # Test input with leap year
+        (
+                [2024, 2024, 2024],
+                [2, 2, 3],
+                [28, 29, 1],
+                [235959, 235959, 0],
+                np.array(['2024-02-28T23:59:59', '2024-02-29T23:59:59', '2024-03-01T00:00:00'], dtype='datetime64[s]')
+        ),
+        # Test input with invalid month (too high)
+        (
+                [2021],
+                [13],
+                [1],
+                [0],
+                ValueError
+        ),
+        # Test input with invalid month (too low)
+        (
+                [2021],
+                [0],
+                [1],
+                [0],
+                ValueError
+        ),
+        # Test input with invalid day (too high)
+        (
+                [2021],
+                [1],
+                [32],
+                [0],
+                ValueError
+        ),
+        # Test input with invalid day (too low)
+        (
+                [2021],
+                [1],
+                [0],
+                [0],
+                ValueError
+        ),
+        # Test input with invalid hhmmss (too high)
+        (
+                [2021],
+                [1],
+                [1],
+                [240000],
+                ValueError
+        ),
+        # Test input with invalid hhmmss (too low)
+        (
+                [2021],
+                [1],
+                [1],
+                [-1],
+                ValueError
+        ),
+        # Test input with empty arrays
+        (
+                [],
+                [],
+                [],
+                [],
+                np.array([], dtype='datetime64[s]')
+        ),
+        # Test input with arrays of different lengths
+        (
+                [2021, 2022],
+                [1, 2, 3],
+                [1, 2, 3],
+                [0, 0, 0],
+                AssertionError
+        ),
+    ]
+)
+def test_datetime_from_ymd_cols(year, month, day, hhmmss, expected):
+    """
+    Test the datetime_from_ymd_cols function for various inputs and expected outputs
+    """
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            datetime_from_ymd_cols(year, month, day, hhmmss)
+    else:
+        # Otherwise, we expect the output to match the expected value
+        result = datetime_from_ymd_cols(year, month, day, hhmmss)
+        # Compare the result to the expected output
+        np.testing.assert_array_equal(result, expected)
+
+# ----
+# from_file_start_end_datetime_GPOD
+# ----
+
+
+@pytest.mark.parametrize("f, df, expected", [
+    # Test with basic input
+    ("/path/to/S3A_GPOD_SAR__SRA_A__20191031T233355_20191101T002424_2019112_IL_v3.proc",
+     pd.DataFrame({"x": np.arange(11)}),
+     np.array(['2019-10-31T23:33:55.000000000', '2019-10-31T23:38:57.900000000',
+               '2019-10-31T23:44:00.800000000', '2019-10-31T23:49:03.700000000',
+               '2019-10-31T23:54:06.600000000', '2019-10-31T23:59:09.500000000',
+               '2019-11-01T00:04:12.400000000', '2019-11-01T00:09:15.300000000',
+               '2019-11-01T00:14:18.200000000', '2019-11-01T00:19:21.100000000',
+               '2019-11-01T00:24:24.000000000'], dtype='datetime64[ns]')),
+
+    # Test with different length of df
+    ("/path/to/S3A_GPOD_SAR__SRA_A__20191031T233355_20191101T002424_2019112_IL_v3.proc",
+     pd.DataFrame({"x": np.arange(6)}),
+     np.array(['2019-10-31T23:33:55.000000000', '2019-10-31T23:44:00.800000000',
+               '2019-10-31T23:54:06.600000000', '2019-11-01T00:04:12.400000000',
+               '2019-11-01T00:14:18.200000000', '2019-11-01T00:24:24.000000000'], dtype='datetime64[ns]')),
+
+    # Test with empty df
+    ("/path/to/S3A_GPOD_SAR__SRA_A__20191031T233355_20191101T002424_2019112_IL_v3.proc",
+     pd.DataFrame({"x": []}),
+     np.array([], dtype='datetime64[ns]')),
+
+    # Test with different filename format
+    ("/path/to/COULD_BE_ANYTHING_HERE__20200101T000000_20200101T010000_202001_RL_v4.proc",
+     pd.DataFrame({"x": np.arange(5)}),
+     np.array(['2020-01-01T00:00:00.000000000', '2020-01-01T00:15:00.000000000',
+               '2020-01-01T00:30:00.000000000', '2020-01-01T00:45:00.000000000',
+               '2020-01-01T01:00:00.000000000'], dtype='datetime64[ns]')),
+])
+def test_from_file_start_end_datetime_GPOD(f, df, expected):
+    result = from_file_start_end_datetime_GPOD(f, df)
+    np.testing.assert_array_equal(result, expected)
+
+
+
+@pytest.mark.parametrize("f, df, expected_exception", [
+    # Test with invalid filename format
+    ("/path/to/invalid_filename_format_20200101T000000_20200101T010000.proc",
+     pd.DataFrame({"x": np.arange(5)}),
+     ValueError),
+
+    # Test with invalid datetime format in filename
+    ("/path/to/S3A_GPOD_SAR__SRA_A__2020-01-01T00:00:00_2020-01-01T01:00:00_202001_IL_v3.proc",
+     pd.DataFrame({"x": np.arange(5)}),
+     ValueError),
+
+    # Test with non-string file path
+    (12345,
+     pd.DataFrame({"x": np.arange(5)}),
+     TypeError),
+
+    # Test with non-DataFrame input
+    ("/path/to/S3A_GPOD_SAR__SRA_A__20200101T000000_20200101T010000_202001_IL_v3.proc",
+     {"x": np.arange(5)},
+     AssertionError),
+])
+def test_from_file_start_end_datetime_GPOD_errors(f, df, expected_exception):
+    with pytest.raises(expected_exception):
+        from_file_start_end_datetime_GPOD(f, df)
+
+
+# ----
+# from_file_start_end_datetime_SARAL
+# ----
+
+
+@pytest.mark.parametrize("f, df, expected_output", [
+    # Test with DataFrame input
+    ("/path/to/SARAL_C139_0036_20200331_234125_20200401_003143_CS2mss_IL_v1.proc",
+     pd.DataFrame({"x": np.arange(11)}),
+     np.array(['2020-03-31T23:41:25', '2020-03-31T23:46:26',
+               '2020-03-31T23:51:28', '2020-03-31T23:56:30',
+               '2020-04-01T00:01:32', '2020-04-01T00:06:34',
+               '2020-04-01T00:11:35', '2020-04-01T00:16:37',
+               '2020-04-01T00:21:39', '2020-04-01T00:26:41',
+               '2020-04-01T00:31:43'], dtype='datetime64[s]')),
+    # Test with incorrect file name format
+    ("/path/to/incorrect_format_20200331_234125_20200401_003143_IL_v1.proc",
+     pd.DataFrame({"x": np.arange(11)}),
+     ValueError),
+    # Test with empty dataframe
+    ("/path/to/SARAL_C139_0036_20200331_234125_20200401_003143_CS2mss_IL_v1.proc",
+                                           pd.DataFrame(), AssertionError),
+    # Test with incorrect input types
+    (123, pd.DataFrame({"x": np.arange(11)}), TypeError),
+    ("/path/to/SARAL_C139_0036_20200331_234125_20200401_003143_CS2mss_IL_v1.proc", "123",AssertionError)
+
+])
+def test_from_file_start_end_datetime_SARAL(f, df, expected_output):
+
+    if isinstance(expected_output, type) and issubclass(expected_output, Exception):
+        with pytest.raises(expected_output):
+            result = from_file_start_end_datetime_SARAL(f, df)
+    else:
+        result = from_file_start_end_datetime_SARAL(f, df)
+        np.testing.assert_array_equal(result, expected_output)
+
