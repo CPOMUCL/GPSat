496b2c0b4cff9d3a029d19d7c77af27480d4ae8a
ronaldBuddys <ronaldmaceachern@gmail.com>
2023-05-17 11:07:44 +0100

    :construction: update the local_expert_oi script to handle list of configs, removed local_expert_oi_from_list_of_config

--- examples/local_expert_oi.py
+++ examples/local_expert_oi.py
@@ -18,10 +18,10 @@ import tensorflow as tf
 
 from PyOptimalInterpolation import get_parent_path, get_data_path
 from PyOptimalInterpolation.local_experts import LocalExpertOI
-from PyOptimalInterpolation.utils import get_config_from_sysargv, nested_dict_literal_eval, grid_2d_flatten
+from PyOptimalInterpolation.utils import get_config_from_sysargv, nested_dict_literal_eval, cprint
 
 # change tensorflow warning levels(?)
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # --
 # helper functions
@@ -35,59 +35,91 @@ pd.set_option("display.max_columns", 200)
 # config
 # ---
 
-config = get_config_from_sysargv()
+configs = get_config_from_sysargv()
 
 # assert config is not None, f"config is None, issue reading it in? check argument provided to script"
 # if config not read in - get default / example
-if config is None:
+if configs is None:
 
     config_file = get_parent_path("configs", "example_local_expert_oi.json")
     warnings.warn(f"\n\nconfig is empty / not provided, will just use an example config:\n{config_file}\n\n")
     with open(config_file, "r") as f:
-        config = nested_dict_literal_eval(json.load(f))
+        configs = nested_dict_literal_eval(json.load(f))
 
     # specify paths and input values
-    config["results"]["dir"] = get_parent_path("results", "example")
-    config["results"]["file"] = "ABC_binned_example.h5"
-    config["locations"]["source"] = get_data_path("locations", "example_expert_locations_arctic_no_date.csv")
-    config["data"]["data_source"] = get_data_path("example", "ABC_binned.h5")
-    config["pred_loc"]["df_file"] = get_data_path("locations", "2d_xy_grid_5x5km.csv")
+    configs["results"]["dir"] = get_parent_path("results", "example")
+    configs["results"]["file"] = "ABC_binned_example.h5"
+    configs["locations"]["source"] = get_data_path("locations", "example_expert_locations_arctic_no_date.csv")
+    configs["data"]["data_source"] = get_data_path("example", "ABC_binned.h5")
+    configs["pred_loc"]["df_file"] = get_data_path("locations", "2d_xy_grid_5x5km.csv")
+
+# if configs are not a list, make them into one
+configs = [configs] if not isinstance(configs, list) else configs
+assert isinstance(configs, list)
 
 
-# ------
-# (extract) parameters
-# ------
+# increment over the list of configs
+t1 = time.time()
+for config_count, config in enumerate(configs):
 
-# pop out and print "comment"
-comment = config.pop("comment", None)
-comment = "\n".join(comment) if isinstance(comment, list) else comment
-print(f"\nconfig 'comment':\n\n{comment}\n\n")
+    # ------
+    # (extract) parameters
+    # ------
 
-results = config["results"]
+    # check for run used kwargs, warn/error if any exists
+    valid_keys = ["comment", "run_kwargs", "misc", "locations", "data", "model", "pred_loc", "results"]
+    invalid_keys = [k for k in config if k not in valid_keys]
+    assert len(invalid_keys) == 0, \
+        f"the following invalid keys were provided in the config:\n{invalid_keys}\nremove and try again"
 
-# run_kwargs - previously named misc
-run_kwargs = config.get("run_kwargs", config.get("misc", {}))
+    cprint("*" * 50, "OKBLUE")
+    cprint("*" * 50, "OKBLUE")
+    cprint("*" * 50, "OKBLUE")
+    print(f"config count: {config_count+1}/{len(configs)}")
 
-# legacy handling of skip_valid_checks_on being in config
-if "skip_valid_checks_on" not in run_kwargs:
-    skip_valid_checks_on = ["skip_valid_checks_on"] + config.get("skip_valid_checks_on", [])
-    run_kwargs["skip_valid_checks_on"] = skip_valid_checks_on
+    # print(json.dumps(json_serializable(config), indent=4))
 
-# --------
-# initialise LocalExpertOI object
-# --------
+    # ------
+    # (extract) parameters
+    # ------
 
-locexp = LocalExpertOI(expert_loc_config=config['locations'],
-                       data_config=config["data"],
-                       model_config=config["model"],
-                       pred_loc_config=config.get("pred_loc", None))
+    # pop out and print "comment"
+    comment = config.pop("comment", None)
+    comment = "\n".join(comment) if isinstance(comment, list) else comment
+    print(f"\nconfig 'comment':\n\n{comment}\n\n")
 
-# ----------------
-# Increment over the expert locations
-# ----------------
+    # run_kwargs - previously named misc
+    run_kwargs = config.get("run_kwargs", config.get("misc", {}))
 
-store_path = os.path.join(results["dir"], results["file"])
+    # legacy handling of skip_valid_checks_on being in config
+    if "skip_valid_checks_on" not in run_kwargs:
+        skip_valid_checks_on = ["skip_valid_checks_on"] + config.get("skip_valid_checks_on", [])
+        run_kwargs["skip_valid_checks_on"] = skip_valid_checks_on
 
-locexp.run(store_path=store_path,
-           **run_kwargs)
+    # --------
+    # initialise LocalExpertOI object
+    # --------
 
+    locexp = LocalExpertOI(expert_loc_config=config['locations'],
+                           data_config=config["data"],
+                           model_config=config["model"],
+                           pred_loc_config=config.get("pred_loc", None))
+
+    # ----------------
+    # Increment over the expert locations
+    # ----------------
+
+    # legacy handling of how to get store_path
+    if "results" in config:
+        results = config["results"]
+        store_path = os.path.join(results["dir"], results["file"])
+    else:
+        store_path = run_kwargs.pop("store_path")
+
+    locexp.run(store_path=store_path,
+               **run_kwargs)
+
+    print(f"results were written to:\n{store_path}")
+
+t2 = time.time()
+print(f"Total run time: {t2 - t1:.2f} seconds")
