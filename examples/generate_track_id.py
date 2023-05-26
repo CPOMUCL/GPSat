f2e86869f16220b7b533dbdc2db16a34fb1a7383
ronaldBuddys <ronaldmaceachern@gmail.com>
2023-05-26 14:24:15 +0100

    :recycle: :construction: refactor code for generating track ids (still work in progress)

--- examples/generate_track_id.py
+++ examples/generate_track_id.py
@@ -61,21 +61,113 @@ def diff_distance(x, p=2, k=1):
 
     return out
 
+
+# ----
+# helper function
+# ----
+
+def assign_tracks(_, delta_measure="dt", cut_off=2e6, plot_vals=True, plot_first=1000000, title=""):
+    assert delta_measure in ["dt", "dr"]
+
+    # _ = df  # .copy(True)
+
+    # create a distance (space or time) to next observation
+    x = _[['x', 'y']].values
+
+    # distance in space
+    _['dr'] = diff_distance(x, p=2, k=1)
+    # distance in time
+    _['dt'] = diff_distance(_['t'].values, p=2, k=1)
+
+    # ------
+    # visualise the deltas - in order to determine the delta cut off size for a new track
+    # ------
+
+    if plot_vals:
+        print("visualise")
+        # NOTE: time differences seem to be unreliable for
+        # gpod_all_rows.h5
+        # - would expect there to be a large jump - review how they a parsed
+        dtime = _['datetime'][:plot_first]
+        xmin, xmax = dtime.min(), dtime.max()
+
+        plt.plot(dtime, _['dt'][:plot_first])
+        plt.hlines(y=cut_off, xmin=xmin, xmax=xmax)
+        plt.title(f"time delta\n{title}")
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.show()
+
+        plt.plot(dtime, _['dr'][:plot_first])
+        plt.hlines(y=cut_off, xmin=xmin, xmax=xmax)
+        plt.title(f"distance delta\n{title}")
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.show()
+
+    # ---
+    # id tracks
+    # ---
+
+    # based on time delta
+    _['track'] = guess_track_num(_[delta_measure].values, cut_off).astype(int)
+
+    # ---
+    # add track by start_date: 'deals' with tracks spanning multiple dates
+    # ---
+
+    if add_by_track_date:
+        # starting datetime of each track
+        tsd = pd.pivot_table(_,
+                             index=['track'],
+                             values=['datetime'],
+                             aggfunc='min').reset_index()
+        tsd.rename(columns={"datetime": "start_date"}, inplace=True)
+        tsd['start_date'] = tsd['start_date'].astype("datetime64[D]")
+
+        # for each start_date get the track number
+        tsd[['start_date', 'track']].drop_duplicates()
+
+        sd = tsd['start_date'].values.astype('int')
+
+        # track number for given start date
+        tsd['track_start'] = track_num_for_date(sd).astype(int)
+
+        _ = _.merge(tsd,
+                    on=['track'],
+                    how='left')
+
+    return _
+
+
 # ----
 # parameters
 # -----
 
+# TODO: be more memory efficient - copy less (add columns and then fill them)
+# TODO: change this to read in from config
 # TODO: add ability to further subset data, e.g. by each 'sat' - just wrap in for loop
 
 # to get 'daily' track numbers
-add_by_track_date = True
+# add_by_track_date = True
+add_by_track_date = False
+
+# get track by: if None will assumed to be from a single source
+# get_track_by = None
+get_track_by = "sat"
+
+#
+delta_measure = "dt"
+# a jump larger than cut_off for delta_measure will signify a new track has started
+cut_off = 2e6
 
 # data_file = get_data_path("RAW", "sats_ra_cry_processed_arco.h5")
 # data_file = get_data_path("RAW", "gpod_all_rows.h5")
 # data_file = get_data_path("RAW", "gpod_lead.h5")
 # data_file = get_data_path("RAW", "sats_ra_cry_processed_arco_all_elev.h5")
-data_file = "/mnt/m2_red_1tb/Data/CSAO/SAR_A.h5"
+# data_file = "/mnt/m2_red_1tb/Data/CSAO/SAR_A.h5"
 # data_file = "/mnt/m2_red_1tb/Data/CSAO/SIN.h5"
+data_file = "/mnt/m2_red_1tb/Data/GPOD/gpod_processed_no_category.h5"
 
 # table = "_data_batches"
 table = "data"
@@ -88,14 +180,18 @@ assert out_table != table
 # sort_by = ['sat', 'datetime']
 sort_by = ['datetime']
 
-# lon, lat = 'lon', 'lat'
-lon_col, lat_col = 'lon_20_ku', 'lat_20_ku'
-lat_0, lon_0 = -90, 0
+lon_col, lat_col = 'lon', 'lat'
+lat_0, lon_0 = 90, 0
+# lon_col, lat_col = 'lon_20_ku', 'lat_20_ku'
+# lat_0, lon_0 = -90, 0
 
 # for plotting
-pole = "south"
+pole = "north"
+# pole = "south"
 # set to None to skip plotting
-plot_before = "2019-01-15"
+# plot_before = "2019-01-15"
+
+# NOTE: read in data (df) is expected to have 'datetime' column
 
 # ---
 # read in data
@@ -105,6 +201,7 @@ pd.set_option("display.max_columns", 200)
 
 assert os.path.exists(data_file)
 
+# TODO: replace with DataLoader.load() so can add column, run col_funcs
 print(f"reading in {table}")
 with pd.HDFStore(data_file, mode="r") as store:
     print(store.keys())
@@ -116,6 +213,9 @@ with pd.HDFStore(data_file, mode="r") as store:
         run_info = storer.attrs['run_info']
     except Exception as e:
         print(e)
+
+print("read in data with head:")
+print(df.head(2))
 
 # --
 # prep data
@@ -137,101 +237,44 @@ df['t'] = df['datetime'].values.astype("datetime64[ms]").astype(float)
 # add x,y coordinates - will use for measuring distance
 df['x'], df['y'] = WGS84toEASE2_New(df[lon_col], df[lat_col], lat_0=lat_0, lon_0=lon_0)
 
-# select a single satellite
-# all_sats = df['sat'].unique()
-# _ = df.loc[df['sat'] == all_sats[0]].copy(True)
+# ----
+# add tracks
+# ----
 
-_ = df  # .copy(True)
+print("adding tracks")
+if get_track_by is None:
+    _ = assign_tracks(df, delta_measure=delta_measure, cut_off=cut_off, plot_vals=True, plot_first=1000000, title="")
+else:
+    tmp = []
+    uvals = df[get_track_by].unique()
+    for uv in uvals:
+        print(f"{get_track_by}: {uv}")
+        _ = df.loc[df[get_track_by] == uv].copy(True)
+        _ = assign_tracks(_, delta_measure=delta_measure, cut_off=cut_off, plot_vals=True, plot_first=1000000, title=f"{get_track_by}: {uv}")
+        tmp.append(_)
+    _ = pd.concat(tmp)
 
-# create a distance (space or time) to next observation
-x = _[['x', 'y']].values
-
-# distance in space
-_['dr'] = diff_distance(x, p=2, k=1)
-# distance in time
-_['dt'] = diff_distance(_['t'].values, p=2, k=1)
-
-# plt.plot(_.iloc[:1000, :]['dt'].values)
+# --
+# drop columns
+# --
 
 # hardcoded: columns just add to drop before writing to file
 drop_columns = ['x', 'y', 't', 'dt', 'dr']
-
-# ------
-# visualise the deltas - in order to determine the delta cut off size for a new track
-# ------
-
-if plot_before is not None:
-    print("visualise")
-    # identify tracks - where there is a large jump in 'distance' between obs
-    # - space distance will be in meters
-    # _.loc[_['dr'] > (50 * 1000)]
-
-    # pick take a subset of dates
-    tmp = _.loc[_['datetime'] < plot_before].copy(True)
-
-    # NOTE: time differences seem to be unreliable for
-    # gpod_all_rows.h5
-    # - would expect there to be a large jump - review how they a parsed
-    plt.plot(tmp['datetime'][:10000], tmp['dt'][:10000])
-    plt.title("time diff")
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.show()
-
-    plt.plot(tmp['datetime'], tmp['dr'])
-    plt.title("space diff")
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.show()
-
-# ---
-# id tracks
-# ---
-
-# based on time delta
-_['track'] = guess_track_num(_['dt'].values, 2e6).astype(int)
-# distance delta
-# _['track'] = guess_track_num(_['dr'].values, 2e6)
-
-
-# ---
-# track by start_date
-# ---
-
-if add_by_track_date:
-    # starting datetime of each track
-    tsd = pd.pivot_table(_,
-                         index=['track'],
-                         values=['datetime'],
-                         aggfunc='min').reset_index()
-    tsd.rename(columns={"datetime": "start_date"}, inplace=True)
-    tsd['start_date'] = tsd['start_date'].astype("datetime64[D]")
-
-    # for each start_date get the track number
-    tsd[['start_date', 'track']].drop_duplicates()
-
-    sd = tsd['start_date'].values.astype('int')
-
-    # track number for given start date
-    tsd['track_start'] = track_num_for_date(sd).astype(int)
-
-    _ = _.merge(tsd,
-                on=['track'],
-                how='left')
+_.drop(drop_columns, axis=1, inplace=True)
 
 # --
 # plot tracks
 # --
 
-tmp = _.loc[_['datetime'] < plot_before].copy(True)
+tmp = _.iloc[:1_000_000].copy(True)
 
 utracks = tmp['track'].unique()
 print(f"unique track count: {len(utracks)}")
 
-select_tracks = np.random.choice(utracks, 200, replace=False)
+select_tracks = np.random.choice(utracks, 30, replace=False)
 # select_tracks = utracks[:26]
-# select_bool = tmp['track'].isin(select_tracks).values
-select_bool = tmp['date'].isin(["2019-01-01"])
+select_bool = tmp['track'].isin(select_tracks).values
+# select_bool = tmp['date'].isin(["2019-01-01"])
 
 lat, lon = tmp.loc[select_bool, lat_col].values, tmp.loc[select_bool, lon_col].values
 
@@ -277,11 +320,10 @@ plt.show()
 # write to table
 # ----
 
-_.drop(drop_columns, axis=1, inplace=True)
-
 cprint("writing:", c='OKBLUE')
 print(_.head(2))
 cprint(f"to:\n{data_file}\ntable: '{out_table}'", c='OKBLUE')
+print(f"writing table with: {len(_)} rows")
 with pd.HDFStore(data_file, mode="a") as store:
 
     track_config = {"comment": f"added tracks using data from table: '{table}'"}
